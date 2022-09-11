e3aa27a2fb2acb8b0c6f3edd0bca06b59af5c870
Your Death <65296195+KONTRAWOR@users.noreply.github.com>
2022-09-11 13:41:13 +0300

    Add files via upload

---
+++ MyFirstCulculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    printf("Enter the sign: ");
+    
+    char i;
+    int number;
+    int second;
+    
+    scanf("%c", &i);
+ while( number != 12){   
+    printf("Enter the firtst number: ");
+    scanf("%d", &number);
+
+    printf("Enter the second one: ");
+    scanf("%d", &second);
+    
+    int c; 
+    if (i == '+'){
+        c = number + second;
+        printf("The result is %d \n", c);
+    }
+    else if (i == "-"){
+        c = number - second;
+         printf("The result is %d \n", c);
+    }
+    else if (i == '*'){
+        c = number * second;
+         printf("The result is %d ", c);
+    }
+ }
+}
